60e23af54d122151945b3f4c13f11672ee93f6ba
Ghozy Muhammad Hanzhalah Muchjidi <gliozghozy@gmail.com>
2021-01-23 17:00:17 +0700

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="title">
+      <h1>Today To-Do-List</h1>
+    </div>
+    <div class="hometask">
+      <div v-for="myTask in task" v-bind:key="myTask.$index" class="task">
+        <TaskList v-bind:myTask="myTask" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskList from "./components/TaskList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data: function () {
+    return {
+      task: [
+        {
+          act: "Read a Book",
+          isDone: false,
+        },
+        {
+          act: "Manage Business",
+          isDone: false,
+        },
+        {
+          act: "Go Jogging",
+          isDone: false,
+        },
+        {
+          act: "Learn New Skill",
+          isDone: false,
+        },
+      ],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    TaskList,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "../src/assets/style/style.css";
 </style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="form-check">
+      <label class="form-check-label" for="flexCheckDefault">
+        {{ myTask.act }}
+      </label>
+
+      <input
+        class="form-check-input"
+        type="checkbox"
+        value=""
+        id="flexCheckDefault"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskList",
+  props: ["myTask"],
+};
+</script>
